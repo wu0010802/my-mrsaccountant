f4699c5178306edf0ddcdae769cb5ab8b3e52101
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-12-08 21:14:09 +0800

    deploy bug test2

--- src/main/java/com/example/MrsAccountant/MrsAccountantApplication.java
+++ src/main/java/com/example/MrsAccountant/MrsAccountantApplication.java
@@ -10,15 +10,17 @@ public class MrsAccountantApplication {
 
     public static void main(String[] args) {
 
-        
-        if (System.getenv("SPRING_PROFILES_ACTIVE") == null) {
+        // 獲取 SPRING_PROFILES_ACTIVE 的值，默認為空字符串
+        String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+        if (activeProfile == null || !activeProfile.equals("prod")) {
+            // 加載 .env 文件
             Dotenv dotenv = Dotenv.load();
             dotenv.entries().forEach(entry -> {
                 System.setProperty(entry.getKey(), entry.getValue());
             });
             System.out.println(".env file loaded for local environment.");
         } else {
-            System.out.println("Detected active profile: " + System.getenv("SPRING_PROFILES_ACTIVE"));
+            System.out.println("Detected active profile: " + activeProfile);
         }
 
         SpringApplication.run(MrsAccountantApplication.class, args);
