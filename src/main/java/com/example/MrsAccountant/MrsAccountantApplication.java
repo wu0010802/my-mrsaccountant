3c0e1af708d25fbc671afc28d396eb4f7c8c1cd9
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2025-03-09 19:32:18 +0800

    test rebase main

--- src/main/java/com/example/MrsAccountant/MrsAccountantApplication.java
+++ src/main/java/com/example/MrsAccountant/MrsAccountantApplication.java
@@ -9,7 +9,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MrsAccountantApplication {
-
+    // test rebase main
     public static void main(String[] args) {
 
         String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
